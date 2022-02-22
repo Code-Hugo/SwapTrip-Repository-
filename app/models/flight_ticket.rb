368312f495ae9d9d90fe2c4e9a4e0241aa71d60a
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-22 20:27:47 +0100

    validations

--- app/models/flight_ticket.rb
+++ app/models/flight_ticket.rb
@@ -3,9 +3,9 @@ class FlightTicket < ApplicationRecord
   belongs_to :departure, class_name: 'Airport'
   belongs_to :arrival, class_name: 'Airport'
   has_many :purchase
-  #validates :airline_code, presence: true
-  #validates :ticket_number, presence: true, numericality: { only_integer: true }, length: { is:13 } 
-  #validates :departure_at, presence: true
+  validates :airline_code, presence: true
+  validates :ticket_number, presence: true, numericality: { only_integer: true }, length: { is:13 } 
+  validates :departure_at, presence: true
   validate :fields_a_and_b_are_different
 
   def fields_a_and_b_are_different
