cf81cdf072d6aad895ef27e58d6e8187636ab2d0
arman aminzadeh <arman.aminzadeh@gmail.com>
2022-02-24 21:28:43 +0100

    adding my dashboard route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+  def my_dashboard
+    @user = current_user
+    @my_offers = @user.flight_tickets
+    @my_purchases = @user.purchases
+    console
+  end
 end
