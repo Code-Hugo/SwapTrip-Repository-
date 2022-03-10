d726309fdd4279316b855197738d9daa9a6f200b
tobiasz golian <tobiasz.golian@travelperk.com>
2022-03-10 20:38:39 +0100

    making possible creation of purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,24 @@
 class PurchasesController < ApplicationController
-    def new
-        @purchase = Purchase.new
-        @user = current_user
-        @flight_ticket = FlightTicket.find(params[:flight_ticket_id])
-      end
+
+  def new
+    @purchase = Purchase.new
+    @user = current_user
+    @flight_ticket = FlightTicket.find(params[:flight_ticket_id])
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    @user = current_user
+    if @purchase.save!
+      redirect_to my_dashboard_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:user_id, :flight_ticket_id)
+  end
 end
