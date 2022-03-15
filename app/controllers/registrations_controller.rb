b06423f93efd2770ee191099f250d52660f4ce97
tobiasz golian <tobiasz.golian@travelperk.com>
2022-03-15 18:56:06 +0100

    making difference depending on where user comes from

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
+  # def new
+  #   super
+  #   session[:from_popup] = params[:from_popup]
+  # end
 
+  # def create
+  #   super
+  #   if session[:from_popup]
+  #     store_user_location!
+  #     session.delete(:from_popup)
+  #   end
+  # end
+
+  # private
+
+  # def store_user_location!
+  #   store_location_for(:user, root_path)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'registrations'}
   get "my_dashboard", to: "pages#my_dashboard", as: :my_dashboard
   root to: 'flight_tickets#index'
   resources :flight_tickets, only:[:index, :show, :new, :create, :edit, :update] do
     resources :purchases, only: [ :create, :new ]
   end
   get "status/:id", to: "flight_tickets#status", as: :status
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
